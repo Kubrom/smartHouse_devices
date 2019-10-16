a3305c20930a0725f369f7739d46c0b7cab43ce8
Kubrom <35294099+Kubrom@users.noreply.github.com>
2019-10-16 16:43:16 +0200

    Add files via upload
    i have added  code we used to test runt the smart house

---
+++ sketch_oct16a.ino
@@ -0,0 +1,32 @@
+
+//TEST RUN 2019-10-16
+
+const int PIN_a=12;
+const int PIN_b=13;
+const int PIN_c=11;
+const int PIN_d=8;
+const string  abc;
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+   pinMode(PIN_a, OUTPUT);
+   pinMode(PIN_b, OUTPUT);
+   pinMode(PIN_c, OUTPUT);
+   pinMode(PIN_d, OUTPUT);
+
+}
+
+void loop() {
+
+  if(abc = lightonO
+  // put your main code here, to run repeatedly:
+   digitalWrite(PIN_a, HIGH);
+   digitalWrite(PIN_b, HIGH);
+   digitalWrite(PIN_c, LOW);
+   digitalWrite(PIN_d, LOW);
+   
+   
+
+}
