81fd81475d0e56aa1f5f758ac4b2bbf28e6be186
kubrom <kubrom.mulugheta@gmail.com>
2019-10-21 14:22:23 +0200

    added switch methods and small updates

--- smart_house/smart_house.ino
+++ smart_house/smart_house.ino
@@ -5,7 +5,18 @@ const int PIN_a=12;
 const int PIN_b=13;
 const int PIN_c=11;
 const int PIN_d=8;
+const int PIN_water_leakage = 4;
+const int PIN_stove = 5;
+const int PIN_fire_alarm = 2;
+const int PIN_window = 6;
+
 int incomingCommandByte = 0; // for incoming serial data
+int reading_water_leakage; // used to read switch values
+int reading_stove; // used to read switch values
+int reading_fire_alarm; // used to read switch values
+int reading_window; // used to read switch values
+
+
 
 
 
@@ -19,6 +30,7 @@ void setup() {
    pinMode(PIN_b, OUTPUT);
    pinMode(PIN_c, OUTPUT);
    pinMode(PIN_d, OUTPUT);
+   pinMode(PIN_water_leakage, INPUT);
    
    
    
@@ -26,8 +38,46 @@ void setup() {
 }
 
 void loop() {
+  //Switch water leakage
+   reading_water_leakage = digitalRead(PIN_water_leakage);
+   if(reading_water_leakage == HIGH) {
+   timer2ON();
+    }
+    if(reading_water_leakage == LOW) {
+   timer2OFF();
+      }
+
+//Switch for stove
+    reading_stove = digitalRead(PIN_stove);
+   if(reading_stove == HIGH) {
+  timer2ON();
+    }
+    if(reading_stove == LOW) {
+  timer2OFF();
+      }   
+
+   //Switch for fire alarm
+   reading_fire_alarm = digitalRead(PIN_fire_alarm);
+   if(reading_fire_alarm == HIGH) {
+  timer2ON();
+    }
+    if(reading_fire_alarm == LOW) {
+  timer2OFF();
+      }   
+
+   //Switch for window
+
+    reading_window = digitalRead(PIN_window);
+   if(reading_window == HIGH) {
+  timer2ON();
+    }
+    if(reading_window == LOW) {
+  timer2OFF();
+      }   
+
+
+      //--------------
    while (Serial.available() > 0){
- 
   incomingCommandByte = Serial.read() - '0';
   
     if( incomingCommandByte == 1){
@@ -35,20 +85,28 @@ void loop() {
     incomingCommandByte = 0;
     }
      else if( incomingCommandByte == 2){
-    //indoorLightsOFF();
+    indoorLightsOFF();
     incomingCommandByte = 0;
     }
     else if( incomingCommandByte == 3){
-    //outdoorLightsON();
+    outdoorLightsON();
     incomingCommandByte = 0;
     }
     else if( incomingCommandByte == 4){
     
-    //outdoorLightsON();
+    outdoorLightsOFF();
     incomingCommandByte = 0;
     }
     else if( incomingCommandByte == 5){
-
+    radiatorON();
+    incomingCommandByte = 0;
+    }
+    else if( incomingCommandByte == 6){
+    radiatorOFF();
+    incomingCommandByte = 0;
+    }
+    else if( incomingCommandByte == 8){
+    
     incomingCommandByte = 0;
     }
 }
@@ -74,7 +132,7 @@ void loop() {
    digitalWrite(PIN_b, LOW);
    digitalWrite(PIN_c, HIGH);
    digitalWrite(PIN_d, LOW);
-   Serial.println("indoor Light on");
+   Serial.println("indoor Light off");
   
   }
 
@@ -86,7 +144,7 @@ void loop() {
    digitalWrite(PIN_b, HIGH);
    digitalWrite(PIN_c, HIGH);
    digitalWrite(PIN_d, HIGH);
-   Serial.println("indoor Light on");
+   Serial.println("outdoor Light on");
   }
   
  void outdoorLightsOFF(){
@@ -96,7 +154,7 @@ void loop() {
    digitalWrite(PIN_b, HIGH);
    digitalWrite(PIN_c, HIGH);
    digitalWrite(PIN_d, HIGH);
-   Serial.println("indoor Light on");
+   Serial.println("outdoor Light off");
   }
   
  void radiatorON(){
@@ -144,6 +202,24 @@ void windowOPEN(){
   //method to open the windows
 }
 
+void timer2ON(){
+  //method to turn on timer 2
+   digitalWrite(PIN_a, LOW);
+   digitalWrite(PIN_b, LOW);
+   digitalWrite(PIN_c, LOW);
+   digitalWrite(PIN_d, HIGH);
+   Serial.println("timer 2 Light On");
+}
+
+void timer2OFF(){
+  //method to turn off the timer (2)
+   digitalWrite(PIN_a, HIGH);
+   digitalWrite(PIN_b, LOW);
+   digitalWrite(PIN_c, LOW);
+   digitalWrite(PIN_d, HIGH);
+   Serial.println("timer 2 Light Off");
+}
+
 
 void windowCLOSE(){
   //method to close the windows}
@@ -152,6 +228,7 @@ void windowCLOSE(){
 
 void waterLeakageON(){
   //method to signal water leakage
+  
 }
 
 
