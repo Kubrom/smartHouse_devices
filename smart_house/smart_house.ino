228777d4bd895fd72baed6db01584af999975a81
kubrom <kubrom.mulugheta@gmail.com>
2019-10-21 11:28:55 +0200

    added signature of all the needed method to implement the functionalities of the smart house

---
+++ smart_house/smart_house.ino
@@ -0,0 +1,126 @@
+
+//list of all needed variables
+
+const int PIN_a=12;
+const int PIN_b=13;
+const int PIN_c=11;
+const int PIN_d=8;
+char command[10];
+int commandNumber;
+
+
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("Welcome to the Smart house");
+   pinMode(PIN_a, OUTPUT);
+   pinMode(PIN_b, OUTPUT);
+   pinMode(PIN_c, OUTPUT);
+   pinMode(PIN_d, OUTPUT);
+   
+   
+
+}
+
+void loop() {
+  
+  
+  commandNumber = Serial.read();
+  
+  
+
+  if(commandNumber  == 49){
+    Serial.println(commandNumber);
+ 
+  
+   digitalWrite(PIN_a, HIGH);
+   digitalWrite(PIN_b, HIGH);
+   digitalWrite(PIN_c, LOW);
+   digitalWrite(PIN_d, LOW);
+   Serial.println("indoor Light on");
+   commandNumber =0;
+}
+else if(commandNumber == 1){
+Serial.println("indoor light off");}
+   
+
+}
+ 
+void indoorLightsON(){
+  //method to turn on  indoor lights
+  }
+
+  
+ 
+void indoorLightsOFF(){
+  //method to turn off  indoor lights
+  }
+
+
+void outdoorLightsON(){
+  //method to turn on outdoor lights
+  }
+  
+void outdoorLightsOFF(){
+  //method to turn on outdoor lights
+  }
+  
+void radiatorON(){
+//  method to turn on radiator
+  }
+
+  
+void radiatorOFF(){
+  //method to turn off radiator
+  }
+
+  
+double getInternalTemperature(){
+  //method to get internal temperature
+  }
+  
+double getExternalTemperature(){
+ // method to get external temperature 
+ }
+
+ 
+void stoveON(){
+  //method to turn the stove on
+}
+
+
+void stoveOFF(){
+  //method to turn the stove off
+}
+
+
+void windowOPEN(){
+  //method to open the windows
+}
+
+
+void windowCLOSE(){
+  //method to close the windows}
+}
+
+
+void waterLeakageON(){
+  //method to signal water leakage
+}
+
+
+void waterLeakegeOFF(){
+  //method to turn off water leakage signal
+  }
+  
+
+void fireAlarmON(){
+  //method to turn  fire alarm
+  }
+  
+void fireAlarmOFF(){
+  //method to turn off fire alarm}
+}
